3c79fcb615cfcc7f117f27a6d451e9659a5c9d0c
lubalesuan <lubavable@mail.ru>
2018-10-02 17:49:51 -0400

    meddle with getSkillText

--- IndeedScraper.py
+++ IndeedScraper.py
@@ -4,3 +4,4 @@ from bs4 import BeautifulSoup
 pageIndeed = requests.get("https://www.indeed.com.my/viewjob?jk=adcb21755b9e2c3a&tk=1coo729qlbl55818&from=serp&vjs=3")
 soupIndeed = BeautifulSoup(pageIndeed.content, 'html.parser')
 print (soupIndeed.prettify())
+
--- JobStreetScraper.py
+++ JobStreetScraper.py
@@ -21,17 +21,38 @@ class ScrapeJobStreet:
 				url = "https://www.jobstreet.com.my"+endUrl
 				jobPage = requests.get(url)
 				jobPageSoup = BeautifulSoup(jobPage.content, 'html.parser')
-				# if job skills found on page
+				# if div with job skills found on page
 				if jobPageSoup.find_all(class_="_1dAEn"):
+					# get skills
 					skills = jobPageSoup.find_all(class_="_1dAEn")[0].find_all(class_="_1eKnr")
 					jobPosition = jobPageSoup.find("span",class_="_189iV").text
+					# insert job position in csv row
 					skillRow = [jobPosition]
+					# insert skills in csv row
 					for skill in skills:
 						skillRow.append(skill.text)
 					writer.writerow(skillRow)
-			# csvFile.close()
 
+	def analyzeSkills(self):
+		popularSkillsDict = dict()
+		with open('jobStreet.csv') as csvfile:
+			reader = csv.DictReader(csvfile)
+			# iterate through csv file rows
+			for row in reader:
+				# iterate through skills
+				for x in range (1, len(row)):
+					print row.keys()
+					# if popularSkillsDict.has_key(row[x]):
+					# 	# increment count of skills
+					# 	popularSkillsDict[row[x]] +=1
+					# else:
+					# 	# if doesn't contain key, init count to 1
+					# 	popularSkillsDict[row[x]] = 1
+		print popularSkillsDict
 
 
 scraper = ScrapeJobStreet()
-scraper.getSkillText()
+# scraper.getSkillText()
+scraper.analyzeSkills()
+
+

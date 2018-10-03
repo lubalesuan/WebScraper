af5e7c4dc6517ef806fb554b85acf87bfe167b70
lubalesuan <lubavable@mail.ru>
2018-10-02 20:25:57 -0400

    indeedscraper and analyzeskills method half ready

--- IndeedScraper.py
+++ IndeedScraper.py
@@ -1,6 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 
-pageIndeed = requests.get("https://www.indeed.com.my/viewjob?jk=adcb21755b9e2c3a&tk=1coo729qlbl55818&from=serp&vjs=3")
-soupIndeed = BeautifulSoup(pageIndeed.content, 'html.parser')
-print (soupIndeed.prettify())
+class IndeedScraper:
+	def getSkillText(self):
+		pageNext = True
+		for i in range(0,19):
+			beginUrl = "https://www.indeed.com.my/jobs?q=&l=Malaysia&limit=50"
+			endUrl = ""
+			# get url for pages
+			if i!=0:
+				endUrl="&start="+str(i*50)
+			url = beginUrl+endUrl
+			page = requests.get(url)
+			#  if page doesn't exist, stop iterating
+			if page.status_code!=200:
+				pageNext = false
+				break
+			soup = BeautifulSoup(page.content, 'html.parser')
+			divs = soup.find_all(class_='jobtitle')
+			for div in divs:
+				endUrl = div.find("a").get('href')
+				url = "https://www.indeed.com.my"+endUrl
+				jobPage = requests.get(url)
+				jobPageSoup = BeautifulSoup(jobPage.content, 'html.parser')
+
+scraper = IndeedScraper()
+scraper.getSkillText()
--- JobStreetScraper.py
+++ JobStreetScraper.py
@@ -8,7 +8,9 @@ class ScrapeJobStreet:
 	def getSkillText(self):
 		# init write to file
 		csvFile = open('jobStreet.csv','w')
-		writer = csv.writer(csvFile)
+		fieldnames = ['job_position', 'skills']
+		writer = csv.DictWriter(csvFile, fieldnames = fieldnames)
+		writer.writeheader()
 		# iterate through alphabetized job pages
 		for c in ascii_lowercase:
 			alphabetPages = requests.get("https://www.jobstreet.com.my/en/career-insights/all-roles/browse-"+c)
@@ -21,17 +23,35 @@ class ScrapeJobStreet:
 				url = "https://www.jobstreet.com.my"+endUrl
 				jobPage = requests.get(url)
 				jobPageSoup = BeautifulSoup(jobPage.content, 'html.parser')
-				# if job skills found on page
+				# if div with job skills found on page
 				if jobPageSoup.find_all(class_="_1dAEn"):
+					# get skills
 					skills = jobPageSoup.find_all(class_="_1dAEn")[0].find_all(class_="_1eKnr")
 					jobPosition = jobPageSoup.find("span",class_="_189iV").text
-					skillRow = [jobPosition]
-					for skill in skills:
-						skillRow.append(skill.text)
-					writer.writerow(skillRow)
-			# csvFile.close()
+					skillRow = ",".join([skill.text for skill in skills])
+					writer.writerow({'job_position':jobPosition, 'skills':skillRow})
 
+
+	def analyzeSkills(self):
+		popularSkillsDict = dict()
+		with open('jobStreet.csv') as csvfile:
+			reader = csv.DictReader(csvfile)
+			# iterate through csv file rows
+			for row in reader:
+				# iterate through skills
+				for skill in row['skills']:
+					if popularSkillsDict.has_key(skill):
+						# increment count of skills
+						popularSkillsDict[skill] +=1
+					else:
+						# if doesn't contain key, init count to 1
+						popularSkillsDict[skill] = 1
+		for i in popularSkillsDict:
+			print i, popularSkillsDict[i]
 
 
 scraper = ScrapeJobStreet()
 scraper.getSkillText()
+scraper.analyzeSkills()
+
+
